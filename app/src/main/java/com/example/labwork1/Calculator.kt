e71cf4297f92b8ad1e58885824ad36a7b478ab1c
JuliaBorovets <juliaborovets2001@gmail.com>
2020-01-21 14:38:49 +0200

    Initial commit

---
+++ app/src/main/java/com/example/labwork1/Calculator.kt
@@ -0,0 +1,43 @@
+package com.example.labwork1
+
+import kotlin.math.pow
+
+class Calculator {
+
+    @Throws(ArithmeticException::class)
+    fun calculateFirst(a: Double, b: Double, c: Double, d: Double): String {
+        return if (d == 0.0) {
+            "Неможливо обчислити!"
+        } else {
+            ((d * a).pow(b) + (b * c).pow(1 / d)).toString()
+        }
+    }
+
+    @Throws(ArithmeticException::class)
+    fun calculateSecond(x: Double, r: Double): String{
+        return if (4*x == r*x){
+            "Неможливо обчислити!"
+        } else{
+            ((4*r - r*x) / (4*x - r*x)).toString()
+        }
+    }
+
+
+    @Throws(ArithmeticException::class)
+    fun calculateThird(n: Int, p: Int): Double{
+        var res = 0.0
+        if (n < 0 || p < 0){
+            print("Неможливо обчислити")
+        } else{
+            for (a in 0..n){
+                for (b in 0..p){
+                    res = a.toDouble().pow(b.toDouble()) + b.toDouble().pow(a.toDouble())
+                }
+            }
+        }
+        return res
+
+    }
+
+}
+
---
+++ app/src/main/java/com/example/labwork1/MainActivity.kt
@@ -0,0 +1,113 @@
+package com.example.labwork1
+
+import android.os.Bundle
+import android.text.method.ScrollingMovementMethod
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import java.lang.Exception
+
+
+class MainActivity : AppCompatActivity() {
+
+    private var textView7: TextView? = null
+    private var textView11: TextView? = null
+
+    private var textView12: TextView? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        //Завдання 1
+        val userInputA: EditText = findViewById(R.id.editText)
+        val userInputB: EditText = findViewById(R.id.editText2)
+        val userInputC: EditText = findViewById(R.id.editText4)
+        val userInputD: EditText = findViewById(R.id.editText3)
+
+        val button1: Button = findViewById(R.id.button)
+        textView7 = findViewById(R.id.textView7)
+        textView7?.text = ""
+        textView7?.movementMethod = ScrollingMovementMethod()
+
+        button1.setOnClickListener {
+
+            try {
+
+                val a = userInputA.text.toString().toDouble()
+                val b = userInputB.text.toString().toDouble()
+                val c = userInputC.text.toString().toDouble()
+                val d = userInputD.text.toString().toDouble()
+
+                val resultFirst = Calculator()
+                val res1 = resultFirst.calculateFirst(a, b, c, d).toDouble()
+
+                textView7?.append("%.2f".format(res1))
+                textView7?.append("\n")
+
+            } catch (e: Exception) {
+                textView7?.append("Неможливо! \n")
+            }
+        }
+
+        //Завдання 2
+
+        val userInputR: EditText = findViewById(R.id.editText5)
+        val userInputX: EditText = findViewById(R.id.editText6)
+
+        val button2: Button = findViewById(R.id.button2)
+        textView11 = findViewById(R.id.textView11)
+        textView11?.text = ""
+        textView11?.movementMethod = ScrollingMovementMethod()
+
+        button2.setOnClickListener {
+
+            try {
+
+                val x = userInputX.text.toString().toDouble()
+                val r = userInputR.text.toString().toDouble()
+
+                val resultSecond = Calculator()
+                val res2 = resultSecond.calculateSecond(x, r).toDouble()
+
+                textView11?.append("%.2f".format(res2))
+                textView11?.append("\n")
+
+
+            } catch (e: Exception) {
+                textView11?.append("Неможливо! \n")
+            }
+        }
+
+        //Завдання 3
+
+        val userInputN: EditText = findViewById(R.id.editText9)
+        val userInputP: EditText = findViewById(R.id.editText10)
+
+        val button3: Button = findViewById(R.id.button3)
+        textView12 = findViewById(R.id.textView12)
+        textView12?.text = ""
+        textView12?.movementMethod = ScrollingMovementMethod()
+
+        button3.setOnClickListener {
+
+            try {
+
+                val n = userInputN.text.toString().toInt()
+                val p = userInputP.text.toString().toInt()
+
+                val resultThird = Calculator()
+                val res3 = resultThird.calculateThird(n, p)
+
+                textView12?.append("%.2f".format(res3))
+                textView12?.append("\n")
+
+
+            } catch (e: Exception) {
+                textView12?.append("Неможливо! \n")
+            }
+        }
+    }
+}
